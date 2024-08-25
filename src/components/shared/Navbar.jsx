405322906dba1b7c85ac2e75d620287394b7653c
unknown <sojibjubayer@gmail.com>
2024-08-25 14:03:17 +0600

    link prefetching add to navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import { IoMenu, IoCloseCircleSharp } from "react-icons/io5";
+import dynamic from 'next/dynamic';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-
   const menuRef = useRef(null);
 
   // Handle click outside of the menu
@@ -15,11 +15,9 @@ const Navbar = () => {
       }
     };
 
-    // Attach the event listener
-    document.addEventListener('mousedown', handleClickOutside);
-    
-    // Clean up the event listener
-    return () => document.removeEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
   return (
@@ -31,26 +29,17 @@ const Navbar = () => {
         <div>
           <ul className="flex  md:flex-row flex-col justify-center items-center gap-5 md:gap-20">
             <li>
-              <Link
-                href="/"
-                className="border border-white px-2 py-1 rounded-md hover:bg-white "
-              >
+              <Link href="/" className="border border-white px-2 py-1 rounded-md hover:bg-white">
                 Dashboard
               </Link>
             </li>
             <li>
-              <Link
-                href="/set-budget"
-                className="border border-white px-2 py-1 rounded-md hover:bg-white "
-              >
+              <Link href="/set-budget" className="border border-white px-2 py-1 rounded-md hover:bg-white">
                 Set Budget
               </Link>
             </li>
             <li>
-              <Link
-                href="/expense-insights"
-                className="border border-white px-2 py-1 rounded-md hover:bg-white "
-              >
+              <Link href="/expense-insights" className="border border-white px-2 py-1 rounded-md hover:bg-white">
                 Expense Insights
               </Link>
             </li>
@@ -63,75 +52,55 @@ const Navbar = () => {
         </div>
       </div>
 
-
-
-
-
-
-
       <div>
         <div className="md:hidden ">
-       
-        
           <div className="bg-green-200 flex  items-center text-center">
-          <div
-            onClick={() => setOpen(!open)}
-            className="lg:hidden btn btn-ghost "
-          >
-            {open ? (
-              <IoCloseCircleSharp className="text-2xl" />
-            ) : (
-              <IoMenu className="text-2xl" />
-            )}
+            <div onClick={() => setOpen(!open)} className="lg:hidden btn btn-ghost ">
+              {open ? (
+                <IoCloseCircleSharp className="text-2xl" />
+              ) : (
+                <IoMenu className="text-2xl" />
+              )}
+            </div>
+            <Link href="/" className="ml-2 text-base  font-bold">
+              Personal Finance Tracker
+            </Link>
           </div>
-          <Link href="/" className="ml-2 text-base  font-bold">Personal Finance Tracker</Link>
-          </div>
-        
-          
 
-          <ul ref={menuRef}
+          <ul
+            ref={menuRef}
             className={` ${
               open ? "top-12 -left-1" : "top-12 -left-60"
-            } duration-1000 absolute     text-black menu-sm
-           ml-2 z-[1] p-2  bg-green-200 space-y-5 w-44 py-4 rounded-sm border border-green-500`}
+            } duration-1000 absolute text-black menu-sm ml-2 z-[1] p-2 bg-green-200 space-y-5 w-44 py-4 rounded-sm border border-green-500`}
           >
-                  <li>
-              <Link
-                href="/"
-                className="border border-white px-2 py-1 rounded-md hover:bg-white "
-              >
+            <li>
+              <Link href="/" className="border border-white px-2 py-1 rounded-md hover:bg-white">
                 Dashboard
               </Link>
             </li>
             <li>
-              <Link
-                href="/set-budget"
-                className="border border-white px-2 py-1 rounded-md hover:bg-white "
-              >
+              <Link href="/set-budget" className="border border-white px-2 py-1 rounded-md hover:bg-white">
                 Set Budget
               </Link>
             </li>
             <li>
-              <Link
-                href="/expense-insights"
-                className="border border-white px-2 py-1 rounded-md hover:bg-white "
-              >
+              <Link href="/expense-insights" className="border border-white px-2 py-1 rounded-md hover:bg-white">
                 Expense Insights
               </Link>
             </li>
             <li>
-              <Link
-                href="#"
-                className="border border-white px-2 py-1 rounded-md hover:bg-white "
-              >
+              <Link href="#" className="border border-white px-2 py-1 rounded-md hover:bg-white">
                 Login
               </Link>
             </li>
-            </ul>
+          </ul>
         </div>
       </div>
     </div>
   );
 };
 
-export default Navbar;
+
+export default dynamic(() => Promise.resolve(Navbar), {
+  ssr: false, 
+});
